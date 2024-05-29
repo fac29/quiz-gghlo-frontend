2683506f095a43a9d512f4e2ba8206990cf9bd6e
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-05-29 17:16:24 +0100

    creating React context api

--- src/components/GameState.tsx
+++ src/components/GameState.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+// import React from 'react';
+import { createContext, useState, useContext, ReactNode } from 'react';
 
-export default function GameState() {
-	return <div>GameState</div>;
+type UserScheme = {
+	questions?: Array<object> | null;
+	totalQuestionsAnswered?: number;
+	totalCorrectAnswers?: number;
+	currentCorrectAnswers?: number;
+	currentQuestionsAnswered?: number;
+	currentQuestionsRequested?: number;
+};
+
+export const UserContext = createContext<UserScheme | null>(null);
+
+export default function GameState({ children }: { children: ReactNode }) {
+	const currentUser = useContext(UserContext);
+
+	const [current, setCurrent] = useState<UserScheme | null>(currentUser);
+
+	return (
+		<UserContext.Provider value={{ ...current, ...setCurrent }}>
+			{children}
+		</UserContext.Provider>
+	);
 }
