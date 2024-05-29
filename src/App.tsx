f9e63d1d890af5b72afdb98c57f74598ce1e62eb
thatGui <thatguifonseca@gmail.com>
2024-05-29 11:24:10 +0100

    main style + btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../src/index.css';
+import Button from './components/Button/Button';
 
 function App() {
 	return (
 		<React.Fragment>
 			<div className='main'>
-				<h1>Hello World</h1>
-				<p>yolo</p>
+				<h1>best team in the history of FAC</h1>
+				<Button
+					btnclassName='btnPrimary'
+					btnText='yolo'
+					btnonClick={() => false}
+				/>
 			</div>
 		</React.Fragment>
 	);
